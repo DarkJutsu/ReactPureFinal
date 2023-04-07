6b9806a980c3fe0bb89b2d0c0f754fc65dc4c089
DarkJutsu <ulloajose34@gmail.com>
2023-04-07 03:09:45 -0600

    prueba config-v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const App = () => {
   return (
     <div>
-      <h1>Proyecto Final</h1>
+      <h1>Proyecto Final - Configurando desde cero!!!</h1>
     </div>
   );
 };
